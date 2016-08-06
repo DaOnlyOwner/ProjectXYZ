fffa78098ce2afaa7015e3eaece6169a60075b43
xGeee <wdal@hush.com>
2016-08-06 16:31:16 +0200

    added EndBehaviour to SpraySpell

--- Source/ProjectXYZ/SpraySpell.cpp
+++ Source/ProjectXYZ/SpraySpell.cpp
@@ -22,6 +22,11 @@ void ASpraySpell::BeginPlay()
 	
 }
 
+void ASpraySpell::EndBehavior()
+{
+	Destroy();
+
+}
 void ASpraySpell::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if (OtherActor->IsA(APlayerCharacter::StaticClass()) && OtherActor != this)
